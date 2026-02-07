81f4e433f12278ee54f9e9548054e6992c76a334
Werkgeist <werkgeist@gmail.com>
2026-02-07 15:00:17 +0000

    Deploy to GitHub Pages

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE = 'mvv-v1';
+const ASSETS = ['./index.html', './manifest.json'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(ASSETS)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(caches.keys().then(keys =>
+    Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+  ));
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+
+  // API calls: network-first
+  if (url.hostname === 'www.mvv-muenchen.de') {
+    e.respondWith(
+      fetch(e.request)
+        .then(res => {
+          const clone = res.clone();
+          caches.open(CACHE).then(c => c.put(e.request, clone));
+          return res;
+        })
+        .catch(() => caches.match(e.request))
+    );
+    return;
+  }
+
+  // Static assets: cache-first
+  e.respondWith(
+    caches.match(e.request).then(r => r || fetch(e.request))
+  );
+});
